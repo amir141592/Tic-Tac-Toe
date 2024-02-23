1f31c91a9c9d5fb4e8a5f019d37b09f304da2754
Amir Allahdadian <amir.allahdadian@gmail.com>
2024-02-23 10:58:11 +0330

    now you can store input value and update the view

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,28 +2,35 @@ import { useState } from "react";
 import "./Player.css";
 
 export default function Player({ name, symbol }) {
+	const [playerName, setPlayerName] = useState(name);
 	const [isEditing, setIsEditing] = useState(false);
 
 	const toggleEdit = () => {
-		isEditing ? setIsEditing(false) : setIsEditing(true);
+		setIsEditing((editing) => !editing);
 	};
 
-	let playername = !isEditing ? (
-		<span className="player-name">{name}</span>
+	const updatePlayerName = (event) => {
+		setPlayerName(event.target.value);
+	};
+
+	let playerNameField = !isEditing ? (
+		<span className="player-name">{playerName}</span>
 	) : (
 		<input
 			type="text"
+			value={playerName}
 			required
+			onChange={updatePlayerName}
 		/>
 	);
 
 	return (
 		<li>
 			<span className="player">
-				{playername}
+				{playerNameField}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button onClick={toggleEdit}>Edit</button>
+			<button onClick={toggleEdit}>{!isEditing ? "Edit" : "Save"}</button>
 		</li>
 	);
 }
